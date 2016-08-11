a120803a54ffac3a27afb470fc4ef12efa6d7d43
clover7 <heaun37@gmail.com>
2016-08-11 10:39:03 +0900

    webrtc sample 적용

---
+++ www/js/video-main.js
@@ -0,0 +1,31 @@
+var app = angular.module('starter.video-main', [])
+
+
+'use strict';
+
+navigator.getUserMedia = navigator.getUserMedia ||
+  navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {
+  audio: false,
+  video: true
+};
+
+var video = document.querySelector('video');
+
+function successCallback(stream) {
+  console.log('successCallback stream: ' + stream);
+  window.stream = stream; // stream available to console
+
+  if (window.URL) {
+    video.src = window.URL.createObjectURL(stream);
+  } else {
+    video.src = stream;
+  }
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);

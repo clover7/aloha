fc03a3f1fb598efa2dd6273079dfea970520f10b
clover7 <heaun37@gmail.com>
2016-09-02 18:29:12 +0900

    web rtc 적용

--- www/js/video-main.js
+++ www/js/video-main.js
@@ -29,3 +29,71 @@ function errorCallback(error) {
 }
 
 navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+
+//custom
+function resizeWindow(target){
+  var deviceHeight = $(window).height();
+  console.log("deviceHeight : " + deviceHeight);
+  // alert("video height : " + $('#video').height());
+  $('body').height(deviceHeight);
+  console.log("bodyHeight : " + $('body').height());
+
+  var bodyHeight = $('body').height();
+  var videoHeight = bodyHeight * 0.7;;
+  $('#video').height(videoHeight);
+  console.log('videoHeight : ' + $('#video').height());
+
+  var deviceWidth = $(window).width();
+
+  if(deviceWidth>560){
+    $('.videoScreen').addClass('video');
+    $('#callTime').addClass('white');
+    $('#video').removeClass('video');
+
+  }
+  else{
+    $('.videoScreen').removeClass('video');
+    $('#callTime').removeClass('white');
+    $('#video').addClass('video');
+  }
+}
+
+function init(){
+  $('#callTime').hide();
+  $('#calleeImg').hide();
+  $('#calleeName').show();
+}
+//
+//$(window).resize(function(){
+//  resizeWindow(this);
+//});
+
+$(document).ready(function() {
+  //resizeWindow(this);
+
+  init();
+
+  $('button').click(function(){
+    var getId = $(this).attr('id');
+
+    switch (getId){
+      case 'startButton':
+        console.log(getId);
+        $('#'+getId).hide();
+        $('#calleeName').hide();
+        $('#callTime').show();
+        $('#calleeImg').show();
+        //   $("#header").addClass('call-accept');
+        break;
+      case 'endButton':
+        console.log(getId);
+        $('#callTime').hide();
+        $('#calleeImg').hide();
+        $('#startButton').show();
+        $('#calleeName').show();
+        break;
+    }
+  });
+
+});
